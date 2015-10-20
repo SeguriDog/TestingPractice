92955355e9898e2a4a1e055e8cbb13216ab486ea
Mauricio Mejía Castro <jemmejiaca@unal.edu.co>
2015-10-19 22:06:44 -0500

    Se escribe el test de unidad para CursoController

--- test/unit/testingpractice/CursoControllerSpec.groovy
+++ test/unit/testingpractice/CursoControllerSpec.groovy
@@ -8,5 +8,15 @@ import spock.lang.*
 @Mock(Curso)
 class CursoControllerSpec extends Specification {
 
+	def "test para numeroDeInstacias"() {
+		when:
+		Curso c1 = new Curso(codigo:2345, nombre:'Mecanica Celeste IV', creditos:12, descripcion:'Esta materia es breve, relajese')
+		Curso c2 = new Curso(codigo:2347, nombre:'Confeccion y Moda', creditos:1, descripcion:'Mejor cancele')
+		c1.save()
+		c2.save()
+		
+		then:
+		controller.numeroDeInstancias() == 2
+	}
 
 }
