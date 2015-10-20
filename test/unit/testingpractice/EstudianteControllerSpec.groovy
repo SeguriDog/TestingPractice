11e532b400e4acceaadbb1cfcafa96208b4a6c68
Mauricio Mejía Castro <jemmejiaca@unal.edu.co>
2015-10-19 21:56:26 -0500

    Se escribe el test de unidad para EstudianteController

--- test/unit/testingpractice/EstudianteControllerSpec.groovy
+++ test/unit/testingpractice/EstudianteControllerSpec.groovy
@@ -7,5 +7,15 @@ import spock.lang.*
 @TestFor(EstudianteController)
 @Mock(Estudiante)
 class EstudianteControllerSpec extends Specification {
+	def "test para numeroDeInstacias"() {
+		when:
+		Estudiante e1 = new Estudiante(nombre:'Carpoforo', apellido:'Guacaneme', edad:27, genero:'M', 6475834, semestre:7, codigoInst:1345236)
+		Estudiante e2 = new Estudiante(nombre:'Bernabe', apellido:'Piraquive', edad:26, genero:'M', 6475857856, semestre:7, codigoInst:957856)
+		e1.save()
+		e2.save()
+		
+		then:
+		controller.numeroDeInstancias() == 2
+	}
 
 }
