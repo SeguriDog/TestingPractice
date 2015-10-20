ba519a62fba8fa16e07e12b5151c873876128120
Mauricio Mejía Castro <jemmejiaca@unal.edu.co>
2015-10-19 22:01:17 -0500

    Se escribe el test de unidad para ProfesorController

--- test/unit/testingpractice/ProfesorControllerSpec.groovy
+++ test/unit/testingpractice/ProfesorControllerSpec.groovy
@@ -8,5 +8,15 @@ import spock.lang.*
 @Mock(Profesor)
 class ProfesorControllerSpec extends Specification {
 
+	def "test para numeroDeInstacias"() {
+		when:
+		Profesor p1 = new Profesor(nombre:'Carpoforo', apellido:'Guacaneme', edad:27, genero:'M', 6475834, oficina:103)
+		Profesor p2 = new Profesor(nombre:'Bernabe', apellido:'Piraquive', edad:26, genero:'M', 6475857856, oficina:405)
+		p1.save()
+		p2.save()
+		
+		then:
+		controller.numeroDeInstancias() == 2
+	}
 
 }
